a020099a99ee82d786e5ac9ac2d39ad2ee9fd9f3
Paweł Data <s173276@v-ie.uek.krakow.pl>
2015-02-20 23:22:11 +0100

    dodany czysty pdjd/ShopBundle

---
+++ src/pdjd/ShopBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace pdjd\ShopBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('pdjdShopBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ src/pdjd/ShopBundle/pdjdShopBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace pdjd\ShopBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class pdjdShopBundle extends Bundle
+{
+}
