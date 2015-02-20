e0a0c66c929e74b1d82c310f74472b6d99081711
Paweł Data <s173276@v-ie.uek.krakow.pl>
2015-02-21 00:48:24 +0100

    FOSUserBundle basic install

--- web/app_dev.php
+++ web/app_dev.php
@@ -18,7 +18,6 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
 */
-
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 Debug::enable();
 
